7128e0102fbb16ff793a6cfe4378ef508ed59f67
Jacek <jacek.dogiel@gmail.com>
2023-09-15 15:08:25 +0200

    Initial Commit

---
+++ RegistrationApp/RegistrationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RegistrationAppApp.swift
+//  RegistrationApp
+//
+//  Created by Jacek Dogiel on 15/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct RegistrationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
